d5eeed82c2169d612c6d413292eccf7dfa53bf5d
virusal7rbi <fahadalwasidi@gmail.com>
2022-11-12 11:40:49 +0300

    lite change

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SafeArea(child: ProfilePage()),
+      home: SafeArea(child: LandingPage()),
     );
   }
 }
